6d228be5626f77057b0f186a0f0ccee0ddd6ecd3
yazankittaneh <kittaneh17@grinnell.edu>
2015-08-29 02:59:43 -0500

    added non-working code to project to start navebar. Added additional dependancy

--- Loggia/app/src/main/java/com/loggia/Feed/EventFeedActivity.java
+++ Loggia/app/src/main/java/com/loggia/Feed/EventFeedActivity.java
@@ -22,6 +22,12 @@ import com.dexafree.materialList.cards.BigImageCard;
 import com.dexafree.materialList.controller.RecyclerItemClickListener;
 import com.dexafree.materialList.model.CardItemView;
 import com.dexafree.materialList.view.MaterialListView;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.DrawerBuilder;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem;
 import com.parse.FindCallback;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
@@ -72,11 +78,36 @@ public class EventFeedActivity extends AppCompatActivity {
         swipeLayout.setColorSchemeResources(R.color.ColorPrimary);
         toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
         setSupportActionBar(toolbar);
-
+        new DrawerBuilder().withActivity(this).build();
         mListView.getLayoutManager().offsetChildrenVertical(10);
 
         updateEvents();
         /* SETUP */
+
+        /* NavBar */
+        //if you want to update the items at a later time it is recommended to keep it in a variable
+        PrimaryDrawerItem item1 = new PrimaryDrawerItem().withName("Home").withIdentifier(1);
+        SecondaryDrawerItem item2 = new SecondaryDrawerItem().withName("Settings").withIdentifier(2);
+
+//create the drawer and remember the `Drawer` result object
+        Drawer result = new DrawerBuilder()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .addDrawerItems(
+                        item1,
+                        new DividerDrawerItem(),
+                        item2,
+                        new SecondaryDrawerItem().withName("Settings")
+                )
+                .withOnDrawerItemClickListener(new Drawer.OnDrawerItemClickListener() {
+                    @Override
+                    public boolean onItemClick(View view, int position, IDrawerItem drawerItem) {
+                        // do something with the clicked item :D
+                    }
+                })
+                .build();
+
+        /* NavBar*/
 
 
         /* LISTENERS */
