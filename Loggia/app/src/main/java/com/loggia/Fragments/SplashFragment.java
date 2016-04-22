2bd2aeced2176392ac242c3739b1b8440d57323c
yazankittaneh <kittaneh17@grinnell.edu>
2016-04-22 19:29:16 +0200

    splash fragments to be used later

---
+++ Loggia/app/src/main/java/com/loggia/Fragments/SplashFragment.java
@@ -0,0 +1,16 @@
+package com.loggia.Fragments;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.loggia.R;
+
+
+public class SplashFragment extends Fragment {
+
+}
